0ceb060ec9b631ded5e717a6c52294be57459371
Jan Solanti <jhs@psonet.com>
2013-04-20 15:46:56 +0300

    Updated events test to work with multiple windows.
    Press return to create a new window (up to 16 windows supported)
    Events now print clock values.

--- test/glwt_events.c
+++ test/glwt_events.c
@@ -8,6 +8,11 @@
 #include <GLXW/glxw.h>
 #endif
 
+static GLWTWindow *windows[16] = {0};
+static int windowsOpen = 0;
+
+static int newWindow();
+
 static void error_callback(const char *msg, void *userdata)
 {
     (void)userdata;
@@ -16,62 +21,98 @@ static void error_callback(const char *msg, void *userdata)
 
 static void window_callback(GLWTWindow *window, const GLWTWindowEvent *event, void *userdata)
 {
-    (void)window;
     (void)userdata;
 
     switch(event->type)
     {
         case GLWT_WINDOW_CLOSE:
-            printf("Window closed\n");
+            printf("[%llu] Window %p closed\n", glwtGetTime(), window);
             break;
         case GLWT_WINDOW_EXPOSE:
-            printf("Window exposed\n");
+            printf("[%llu] Window %p exposed\n", glwtGetTime(), window);
             {
+                glwtMakeCurrent(window);
+                glxwInit();
                 glClearColor(0.2f, 0.4f, 0.7f, 1.0f);
                 glClear(GL_COLOR_BUFFER_BIT);
                 glwtSwapBuffers(window);
+                glwtMakeCurrent(NULL);
             }
             break;
         case GLWT_WINDOW_RESIZE:
-            printf("Window resized  width: %d  height: %d\n", event->resize.width, event->resize.height);
+            printf("[%llu] Window %p resized  width: %d  height: %d\n", glwtGetTime(), window, event->resize.width, event->resize.height);
             break;
         case GLWT_WINDOW_SHOW:
         case GLWT_WINDOW_HIDE:
-            printf("Window %s\n", (event->type == GLWT_WINDOW_SHOW) ? "show" : "hide");
+            printf("[%llu] Window %p %s\n", glwtGetTime(), window, (event->type == GLWT_WINDOW_SHOW) ? "show" : "hide");
             break;
         case GLWT_WINDOW_FOCUS_IN:
         case GLWT_WINDOW_FOCUS_OUT:
-            printf("Window focus %s\n", (event->type == GLWT_WINDOW_FOCUS_IN) ? "in" : "out");
+            printf("[%llu] Window %p focus %s\n", glwtGetTime(), window, (event->type == GLWT_WINDOW_FOCUS_IN) ? "in" : "out");
             break;
         case GLWT_WINDOW_KEY_UP:
         case GLWT_WINDOW_KEY_DOWN:
-            printf("Key %s  keysym: 0x%x  scancode: %d  mod: %X\n",
+            printf("[%llu] Window %p key %s  keysym: 0x%x  scancode: %d  mod: %X\n", glwtGetTime(), window,
                 (event->type == GLWT_WINDOW_KEY_DOWN) ? "down" : "up",
                 event->key.keysym, event->key.scancode, event->key.mod);
+            if(event->type == GLWT_WINDOW_KEY_DOWN && event->key.keysym == GLWT_KEY_RETURN)
+            {
+                newWindow();
+            }
             break;
         case GLWT_WINDOW_BUTTON_UP:
         case GLWT_WINDOW_BUTTON_DOWN:
-            printf("Button %s  x: %d  y: %d  button: %d  mod: %X\n",
+            printf("[%llu] Window %p button %s  x: %d  y: %d  button: %d  mod: %X\n", glwtGetTime(), window,
                 (event->type == GLWT_WINDOW_BUTTON_DOWN) ? "down" : "up",
                 event->button.x, event->button.y, event->button.button, event->button.mod);
             break;
         case GLWT_WINDOW_MOUSE_MOTION:
-            printf("Motion  x: %d  y: %d  buttons: %X\n",
+            printf("[%llu] Window %p motion  x: %d  y: %d  buttons: %X\n", glwtGetTime(), window,
                 event->motion.x, event->motion.y, event->motion.buttons);
             break;
         case GLWT_WINDOW_MOUSE_ENTER:
         case GLWT_WINDOW_MOUSE_LEAVE:
-            printf("Mouse %s\n", (event->type == GLWT_WINDOW_MOUSE_ENTER) ? "enter" : "leave");
+            printf("[%llu] Window %p mouse %s\n", glwtGetTime(), window, (event->type == GLWT_WINDOW_MOUSE_ENTER) ? "enter" : "leave");
             break;
         default:
             break;
     }
 }
 
+static int newWindow()
+{
+    int width, height;
+    int i, id = 0;
+    for (i = 0; i < 16; ++i)
+    {
+        if(!windows[i])
+        {
+            windows[i] = glwtWindowCreate("", 400, 300, NULL, window_callback, NULL);
+            if(!windows[i])
+            {
+                printf("Could not open a window!\n");
+                return 0;
+            }
+            id = i;
+            break;
+        }
+    }
+
+    ++windowsOpen;
+
+    glwtWindowSetTitle(windows[id], "GLWT Events test");
+
+    glwtWindowGetSize(windows[id], &width, &height);
+    printf("[%llu] Window %p size: %d x %d\n", glwtGetTime(), windows[id], width, height);
+
+    glwtWindowShow(windows[id], 1);
+
+    return 1;
+}
+
 int main(int argc, char *argv[])
 {
-    int err = -1;
-    GLWTWindow *window = 0;
+    int i, err = -1;
     GLWTConfig glwt_config = {
         0, 0, 0, 0,
         0, 0,
@@ -85,40 +126,39 @@ int main(int argc, char *argv[])
 #endif
     };
 
-    int width, height;
-
     (void)argc; (void)argv;
 
     if(glwtInit(&glwt_config, error_callback, NULL) != 0)
         goto error;
 
-    if(!(window = glwtWindowCreate("", 400, 300, NULL, window_callback, NULL)))
+    if(!newWindow())
         goto error;
 
-    glwtWindowSetTitle(window, "GLWT Events test");
-
-    glwtWindowShow(window, 1);
-    glwtMakeCurrent(window);
-    glwtSwapInterval(window, 1);
-
-    glxwInit();
-
-    printf("%s\n", (const char*)glGetString(GL_VERSION));
-
-    glwtWindowGetSize(window, &width, &height);
-    printf("Window size: %d x %d\n", width, height);
-
-    while(!glwtWindowClosed(window))
+    while(windowsOpen)
     {
         if(glwtEventHandle(1) != 0)
             goto error;
+
+        for (i = 0; i < 16; ++i)
+        {
+            if(windows[i] && glwtWindowClosed(windows[i]))
+            {
+                glwtWindowDestroy(windows[i]);
+                --windowsOpen;
+                windows[i] = 0;
+            }
+        }
     }
 
     glwtMakeCurrent(0);
 
     err = 0;
 error:
-    glwtWindowDestroy(window);
+    for (i = 0; i < 16; ++i)
+    {
+        if(windows[i])
+            glwtWindowDestroy(windows[i]);
+    }
     glwtQuit();
     return err;
 }
